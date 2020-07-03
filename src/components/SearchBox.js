c4e192404044b1ba3d51d5930ab5462808660056
Samuel Thompson <thomps9012@gmail.com>
2020-07-03 09:26:33 -0400

    added in an employee search

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -3,12 +3,31 @@ import "../styles/SearchBox.css";
 
 function SearchBox({ handleSearchChange }) {
     return (
-      <div className="searchbox">
-        <form className="form-inline">
-         {/* this will be the input box for the searchbox */}
-        </form>
+      <form className="search">
+      <div className="form-group">
+        <label htmlFor="employee">Employee Name:</label>
+        <input
+          value={props.search}
+          onChange={props.handleInputChange}
+          name="employee"
+          list="employees"
+          type="text"
+          className="form-control"
+          placeholder="Type in an employee name"
+          id="employee"
+        />
+        <datalist id="employees">
+          {props.employees.map(employee => (
+            <option value={employee} key={employee} />
+          ))}
+        </datalist>
+        <button type="submit" onClick={props.handleFormSubmit} className="btn btn-success">
+          Search
+        </button>
       </div>
+    </form>
     );
   }
   export default SearchBox;
+  
   
